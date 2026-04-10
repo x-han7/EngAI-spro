1ef5991e35272ea503b858a240f2fedb37770736
root <1968114585@qq.com>
2026-04-10 23:29:43 +0800

    feat: 完成全局搜索组件和首页开发

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -7,11 +7,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import focusPlugin from './directives/focus'
 const app = createApp(App) // 创建 App 实例
 const pinia = createPinia() // 创建 Pinia 实例
 pinia.use(piniaPluginPersistedstate) // 使用 Pinia 持久化状态
 app.use(pinia) // 使用 Pinia
 app.use(ElementPlus, { locale: zhCn }) // 使用 ElementPlus
 app.use(router) // 使用 Router
+app.use(focusPlugin) // 使用自定义指令插件
 
 app.mount('#app')
