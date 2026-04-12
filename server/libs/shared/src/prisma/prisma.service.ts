30552775243732e2e3640b5df3cf074b7d615420
root <1968114585@qq.com>
2026-04-12 12:28:54 +0800

    chore: 优化配置管理 - 更新 ESLint 配置规则 - 替换 dotenv 为 @nestjs/config 管理环境变量

--- server/libs/shared/src/prisma/prisma.service.ts
+++ server/libs/shared/src/prisma/prisma.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
-import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-    });
+  constructor(private readonly configService: ConfigService) {
+    const databaseUrl = configService.get('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString: databaseUrl });
     super({
       adapter,
     });
